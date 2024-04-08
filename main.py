28f0b67a9ca030b06af509713eaad444c850483e
Jayden Hendrickse <Jayden.heberhendrickse@gmail.com>
2024-04-08 10:34:39 +0200

    Set up the UI, placing elements on the grid. Still need to add text fields

--- main.py
+++ main.py
@@ -4,3 +4,36 @@ from tkinter import *
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+app = Tk()
+app.title("Password Manager")
+app.config(padx=20, pady=20)
+
+# Logo
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=0, row=0, columnspan=3)
+
+#Label Input pairs
+# website
+website_label = Label(text="Website")
+website_label.grid(column=0, row=1)
+
+# Username or email
+user_label = Label(text="Username or Email")
+user_label.grid(column=0, row=2)
+
+# Password
+password_label = Label(text="Password")
+password_label.grid(column=0, row=3)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+
+add_password_button = Button(text="Add")
+add_password_button.grid(column=1, row=4)
+
+# Mainloop
+app.mainloop()
