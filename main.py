670dd9bb7d788e4d669cfb361772de4efdefd9e7
Jayden Hendrickse <Jayden.heberhendrickse@gmail.com>
2024-04-08 11:49:47 +0200

    Can now save password details to text file

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def save_password_to_file():
+    website = website_entry.get()
+    email = user_entry.get()
+    password = password_entry.get()
+
+    with open("passwords.tx", "a") as file:
+        file.write(f"{website}|{email}|{password}\n")
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 app = Tk()
 app.title("Password Manager")
@@ -28,6 +39,7 @@ user_label = Label(text="Username/Email")
 user_label.grid(column=0, row=2)
 
 user_entry = Entry(width=35)
+user_entry.insert(END, "filler@email.com")
 user_entry.grid(column=1, row=2, columnspan=2, sticky="ew")
 
 # Password
@@ -41,7 +53,7 @@ password_entry.grid(column=1, row=3)
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3)
 
-add_password_button = Button(text="Add", width=36)
+add_password_button = Button(text="Add", width=36, command=save_password_to_file)
 add_password_button.grid(column=1, row=4, columnspan=2)
 
 # Mainloop
